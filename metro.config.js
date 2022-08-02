e059168d572ec318076c49b117a8b8c4fb09ec3a
Aike van den Brink <aikevandenbrink@gmail.com>
2022-08-02 14:20:36 +0200

    Also add the txt extension to the source extensions list

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,5 @@ const { getDefaultConfig } = require('expo/metro-config');
 const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.assetExts.push('txt');
 defaultConfig.resolver.sourceExts.push('cjs');
+defaultConfig.resolver.sourceExts.push('txt');
 module.exports = defaultConfig;
